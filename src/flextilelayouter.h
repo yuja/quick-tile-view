79f44601dd18bc6b66343310cb3e0970735f2729
Yuya Nishihara <yuya@tcha.org>
2021-10-11 23:30:11 +0900

    flex: clean up unused includes

--- src/flextilelayouter.h
+++ src/flextilelayouter.h
@@ -1,5 +1,4 @@
 #pragma once
-#include <QPoint>
 #include <QPointF>
 #include <QQmlContext>
 #include <QQuickItem>
--- src/flextiler.h
+++ src/flextiler.h
@@ -1,5 +1,4 @@
 #pragma once
-#include <QPoint>
 #include <QPointF>
 #include <QPointer>
 #include <QQmlComponent>
