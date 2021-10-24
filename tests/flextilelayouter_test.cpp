b09db1aca8d65f5b1654e888d670e8bc228f89e2
Yuya Nishihara <yuya@tcha.org>
2021-10-24 16:23:13 +0900

    flex: add basic move tests

--- tests/flextilelayouter_test.cpp
+++ tests/flextilelayouter_test.cpp
@@ -5,6 +5,8 @@
 namespace {
 using Tile = FlexTileLayouter::Tile;
 
+constexpr QRectF unitRect { 0.0, 0.0, 1.0, 1.0 };
+
 std::vector<Tile> createTiles(size_t count)
 {
     std::vector<Tile> tiles;
@@ -13,6 +15,14 @@ std::vector<Tile> createTiles(size_t count)
         tiles.push_back({ { 0.0, 0.0, 0.0, 0.0 }, {}, {}, {}, {}, {}, {} });
     }
     return tiles;
+}
+
+void moveBorder(FlexTileLayouter &layouter, size_t index, Qt::Orientations orientations,
+                const QPointF &normPos, const QSizeF &handleSize, const QSizeF &snapSize = {})
+{
+    layouter.startMoving(index, orientations, false, unitRect, handleSize);
+    layouter.moveTo(normPos, snapSize);
+    layouter.resetMovingState();
 }
 }
 
@@ -186,4 +196,170 @@ TEST(FlexTileLayouterTest, Close2ToBottom)
 
     EXPECT_EQ(layouter.tileAt(0).normRect.y0, 0.0);
     EXPECT_EQ(layouter.tileAt(0).normRect.y1, 1.0);
+}
+
+TEST(FlexTileLayouterTest, MoveH2)
+{
+    FlexTileLayouter layouter;
+    layouter.split(0, Qt::Horizontal, createTiles(1), {});
+    ASSERT_EQ(layouter.count(), 2);
+
+    layouter.startMoving(1, Qt::Horizontal, false, unitRect, {});
+    EXPECT_TRUE(layouter.isMoving());
+
+    layouter.moveTo({ 0.2, 0.0 }, {});
+    EXPECT_TRUE(layouter.isMoving());
+
+    EXPECT_EQ(layouter.tileAt(0).normRect.x0, 0.0);
+    EXPECT_EQ(layouter.tileAt(0).normRect.x1, 0.2);
+    EXPECT_EQ(layouter.tileAt(1).normRect.x0, 0.2);
+    EXPECT_EQ(layouter.tileAt(1).normRect.x1, 1.0);
+
+    EXPECT_EQ(layouter.tileAt(0).normRect.y0, 0.0);
+    EXPECT_EQ(layouter.tileAt(0).normRect.y1, 1.0);
+    EXPECT_EQ(layouter.tileAt(0).normRect.y0, 0.0);
+    EXPECT_EQ(layouter.tileAt(0).normRect.y1, 1.0);
+
+    layouter.moveTo({ 0.7, 0.0 }, {});
+    layouter.resetMovingState();
+    EXPECT_FALSE(layouter.isMoving());
+
+    EXPECT_EQ(layouter.tileAt(0).normRect.x0, 0.0);
+    EXPECT_EQ(layouter.tileAt(0).normRect.x1, 0.7);
+    EXPECT_EQ(layouter.tileAt(1).normRect.x0, 0.7);
+    EXPECT_EQ(layouter.tileAt(1).normRect.x1, 1.0);
+
+    EXPECT_EQ(layouter.tileAt(0).normRect.y0, 0.0);
+    EXPECT_EQ(layouter.tileAt(0).normRect.y1, 1.0);
+    EXPECT_EQ(layouter.tileAt(0).normRect.y0, 0.0);
+    EXPECT_EQ(layouter.tileAt(0).normRect.y1, 1.0);
+}
+
+TEST(FlexTileLayouterTest, MoveV2)
+{
+    FlexTileLayouter layouter;
+    layouter.split(0, Qt::Vertical, createTiles(1), {});
+    ASSERT_EQ(layouter.count(), 2);
+
+    layouter.startMoving(1, Qt::Vertical, false, unitRect, {});
+    EXPECT_TRUE(layouter.isMoving());
+
+    layouter.moveTo({ 0.0, 0.3 }, {});
+    EXPECT_TRUE(layouter.isMoving());
+
+    EXPECT_EQ(layouter.tileAt(0).normRect.x0, 0.0);
+    EXPECT_EQ(layouter.tileAt(0).normRect.x1, 1.0);
+    EXPECT_EQ(layouter.tileAt(0).normRect.x0, 0.0);
+    EXPECT_EQ(layouter.tileAt(0).normRect.x1, 1.0);
+
+    EXPECT_EQ(layouter.tileAt(0).normRect.y0, 0.0);
+    EXPECT_EQ(layouter.tileAt(0).normRect.y1, 0.3);
+    EXPECT_EQ(layouter.tileAt(1).normRect.y0, 0.3);
+    EXPECT_EQ(layouter.tileAt(1).normRect.y1, 1.0);
+
+    layouter.moveTo({ 0.0, 0.8 }, {});
+    layouter.resetMovingState();
+    EXPECT_FALSE(layouter.isMoving());
+
+    EXPECT_EQ(layouter.tileAt(0).normRect.x0, 0.0);
+    EXPECT_EQ(layouter.tileAt(0).normRect.x1, 1.0);
+    EXPECT_EQ(layouter.tileAt(0).normRect.x0, 0.0);
+    EXPECT_EQ(layouter.tileAt(0).normRect.x1, 1.0);
+
+    EXPECT_EQ(layouter.tileAt(0).normRect.y0, 0.0);
+    EXPECT_EQ(layouter.tileAt(0).normRect.y1, 0.8);
+    EXPECT_EQ(layouter.tileAt(1).normRect.y0, 0.8);
+    EXPECT_EQ(layouter.tileAt(1).normRect.y1, 1.0);
+}
+
+TEST(FlexTileLayouterTest, MoveH2Clamped)
+{
+    FlexTileLayouter layouter;
+    layouter.split(0, Qt::Horizontal, createTiles(1), {});
+    ASSERT_EQ(layouter.count(), 2);
+
+    moveBorder(layouter, 1, Qt::Horizontal, { 0.0, 0.0 }, { 0.1, 0.1 });
+    EXPECT_EQ(layouter.tileAt(0).normRect.x1, layouter.tileAt(1).normRect.x0);
+    EXPECT_DOUBLE_EQ(layouter.tileAt(0).normRect.x1, 0.1);
+    EXPECT_DOUBLE_EQ(layouter.tileAt(1).normRect.x0, 0.1);
+
+    moveBorder(layouter, 1, Qt::Horizontal, { 1.0, 1.0 }, { 0.1, 0.1 });
+    EXPECT_EQ(layouter.tileAt(0).normRect.x1, layouter.tileAt(1).normRect.x0);
+    EXPECT_DOUBLE_EQ(layouter.tileAt(0).normRect.x1, 0.9);
+    EXPECT_DOUBLE_EQ(layouter.tileAt(1).normRect.x0, 0.9);
+}
+
+TEST(FlexTileLayouterTest, MoveV2Clamped)
+{
+    FlexTileLayouter layouter;
+    layouter.split(0, Qt::Vertical, createTiles(1), {});
+    ASSERT_EQ(layouter.count(), 2);
+
+    moveBorder(layouter, 1, Qt::Vertical, { 0.0, 0.0 }, { 0.1, 0.1 });
+    EXPECT_EQ(layouter.tileAt(0).normRect.y1, layouter.tileAt(1).normRect.y0);
+    EXPECT_DOUBLE_EQ(layouter.tileAt(0).normRect.y1, 0.1);
+    EXPECT_DOUBLE_EQ(layouter.tileAt(1).normRect.y0, 0.1);
+
+    moveBorder(layouter, 1, Qt::Vertical, { 1.0, 1.0 }, { 0.1, 0.1 });
+    EXPECT_EQ(layouter.tileAt(0).normRect.y1, layouter.tileAt(1).normRect.y0);
+    EXPECT_DOUBLE_EQ(layouter.tileAt(0).normRect.y1, 0.9);
+    EXPECT_DOUBLE_EQ(layouter.tileAt(1).normRect.y0, 0.9);
+}
+
+TEST(FlexTileLayouterTest, MoveHV2x2)
+{
+    FlexTileLayouter layouter;
+    layouter.split(0, Qt::Horizontal, createTiles(1), { 0.1, 0.1 });
+    layouter.split(0, Qt::Vertical, createTiles(1), { 0.1, 0.1 });
+    layouter.split(2, Qt::Vertical, createTiles(1), { 0.1, 0.1 });
+    ASSERT_EQ(layouter.count(), 4);
+
+    // All borders are isolated at this moment.
+    moveBorder(layouter, 3, Qt::Horizontal, { 0.4, 0.0 }, { 0.1, 0.1 });
+    EXPECT_EQ(layouter.tileAt(0).normRect.x1, layouter.tileAt(2).normRect.x0);
+    EXPECT_EQ(layouter.tileAt(1).normRect.x1, layouter.tileAt(3).normRect.x0);
+    EXPECT_DOUBLE_EQ(layouter.tileAt(0).normRect.x1, 0.5);
+    EXPECT_DOUBLE_EQ(layouter.tileAt(1).normRect.x1, 0.4);
+    EXPECT_DOUBLE_EQ(layouter.tileAt(2).normRect.x0, 0.5);
+    EXPECT_DOUBLE_EQ(layouter.tileAt(3).normRect.x0, 0.4);
+
+    // Vertical border should get unisolated.
+    moveBorder(layouter, 1, Qt::Vertical, { 0.0, 0.6 }, { 0.1, 0.1 });
+    EXPECT_EQ(layouter.tileAt(0).normRect.y1, layouter.tileAt(1).normRect.y0);
+    EXPECT_EQ(layouter.tileAt(2).normRect.y1, layouter.tileAt(3).normRect.y0);
+    EXPECT_EQ(layouter.tileAt(0).normRect.y1, layouter.tileAt(2).normRect.y1);
+    EXPECT_EQ(layouter.tileAt(1).normRect.y0, layouter.tileAt(3).normRect.y0);
+    EXPECT_DOUBLE_EQ(layouter.tileAt(0).normRect.y1, 0.6);
+    EXPECT_DOUBLE_EQ(layouter.tileAt(1).normRect.y0, 0.6);
+    EXPECT_DOUBLE_EQ(layouter.tileAt(2).normRect.y1, 0.6);
+    EXPECT_DOUBLE_EQ(layouter.tileAt(3).normRect.y0, 0.6);
+}
+
+TEST(FlexTileLayouterTest, MoveVH2x2)
+{
+    FlexTileLayouter layouter;
+    layouter.split(0, Qt::Horizontal, createTiles(1), { 0.1, 0.1 });
+    layouter.split(0, Qt::Vertical, createTiles(1), { 0.1, 0.1 });
+    layouter.split(2, Qt::Vertical, createTiles(1), { 0.1, 0.1 });
+    ASSERT_EQ(layouter.count(), 4);
+
+    // All borders are isolated at this moment.
+    moveBorder(layouter, 1, Qt::Vertical, { 0.0, 0.3 }, { 0.1, 0.1 });
+    EXPECT_EQ(layouter.tileAt(0).normRect.y1, layouter.tileAt(1).normRect.y0);
+    EXPECT_EQ(layouter.tileAt(2).normRect.y1, layouter.tileAt(3).normRect.y0);
+    EXPECT_DOUBLE_EQ(layouter.tileAt(0).normRect.y1, 0.3);
+    EXPECT_DOUBLE_EQ(layouter.tileAt(1).normRect.y0, 0.3);
+    EXPECT_DOUBLE_EQ(layouter.tileAt(2).normRect.y1, 0.5);
+    EXPECT_DOUBLE_EQ(layouter.tileAt(3).normRect.y0, 0.5);
+
+    // Horizontal border should get unisolated.
+    moveBorder(layouter, 2, Qt::Horizontal, { 0.8, 0.0 }, { 0.1, 0.1 });
+    EXPECT_EQ(layouter.tileAt(0).normRect.x1, layouter.tileAt(2).normRect.x0);
+    EXPECT_EQ(layouter.tileAt(1).normRect.x1, layouter.tileAt(3).normRect.x0);
+    EXPECT_EQ(layouter.tileAt(0).normRect.x1, layouter.tileAt(1).normRect.x1);
+    EXPECT_EQ(layouter.tileAt(2).normRect.x0, layouter.tileAt(3).normRect.x0);
+    EXPECT_DOUBLE_EQ(layouter.tileAt(0).normRect.x1, 0.8);
+    EXPECT_DOUBLE_EQ(layouter.tileAt(1).normRect.x1, 0.8);
+    EXPECT_DOUBLE_EQ(layouter.tileAt(2).normRect.x0, 0.8);
+    EXPECT_DOUBLE_EQ(layouter.tileAt(3).normRect.x0, 0.8);
 }
